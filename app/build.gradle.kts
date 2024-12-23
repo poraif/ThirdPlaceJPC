6716da70ea508cfb3cc9f4ad3839b254cc360680
poraif <peadar.oraif@gmail.com>
2024-12-23 18:51:41 +0100

    firebase storage dependencies added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -75,6 +75,7 @@ dependencies {
     implementation(libs.firebase.auth)
     implementation(libs.firebase.firestore)
     implementation(libs.googleid)
+    implementation(libs.firebase.storage)
     ksp(libs.hilt.android.compiler)
     implementation(libs.androidx.hilt.navigation.compose)
 
@@ -98,6 +99,7 @@ dependencies {
     //Firebase
     implementation(libs.firebase.auth.ktx)
     implementation(libs.firebase.firestore)
+    implementation(libs.firebase.storage)
 
     //googleId
     implementation(libs.googleid)
